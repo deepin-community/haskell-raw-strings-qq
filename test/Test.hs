6da73b3d92e1c1f7b87a7d0348c22ec13066b45e
Deepin Developer <deepin-dev@deepin.org>
2022-12-20 14:32:21 +0800

    feat: Init commit
    init haskell-raw-strings-qq
    
    Log:

---
+++ test/Test.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main
+       where
+
+import Test.HUnit
+import Text.RawString.QQ
+import System.Exit
+
+multilineUnixNewlines :: String
+multilineUnixNewlines = [r|FOO
+BAR|]
+
+multilineWindowsNewlines :: String
+multilineWindowsNewlines = [r|FOO
+BAR|]
+
+main :: IO ()
+main = defaultMain $ test [
+  "Windows newlines" ~: (multilineUnixNewlines ~=? multilineWindowsNewlines)
+  ]
+
+defaultMain :: Test -> IO ()
+defaultMain t = do
+  cnts <- runTestTT t
+  case failures cnts + errors cnts of
+    0 -> exitWith $ ExitSuccess
+    n -> exitWith $ ExitFailure n
